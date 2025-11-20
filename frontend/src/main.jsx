06c3527550c72c6507f241184e462655cd19d206
Khaled <khaledkhlifi086@gmail.com>
2025-11-20 20:28:33 +0100

    authentication added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -16,8 +16,7 @@ if (!PUBLISHABLE_KEY) {
 }
 
 createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
-            
+    <BrowserRouter>  
                 <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
                     <Provider store={store}>
                          <App />
