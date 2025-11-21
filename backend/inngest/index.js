3656b62982414bed7501c92bea0654ab5cfe521e
Khaled <khaledkhlifi086@gmail.com>
2025-11-21 20:11:42 +0100

    fixing user creation

--- backend/inngest/index.js
+++ backend/inngest/index.js
@@ -1,4 +1,5 @@
 import { Inngest } from "inngest";
+import prisma from "../configs/prisma.js"
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "my-app" });
@@ -13,7 +14,7 @@ const syncUserCreation = inngest.createFunction(
         await prisma.user.create({
             data : {
                 id: data.id,
-                email: data?.email_addresses[0]?.email_addres,
+                email: data?.email_addresses[0]?.email_address,
                 name: data?.first_name + " " + data?.last_name,
                 image: data?.image_url,
             }
